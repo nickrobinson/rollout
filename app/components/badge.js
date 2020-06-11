73a80f8bcbc0ac364133bf2888dcee936b100be6
Nick Robinson <nrobinson13@gmail.com>
2020-06-10 20:19:24 -0400

    feat: show status badge

--- app/components/badge.js
+++ app/components/badge.js
@@ -2,6 +2,8 @@ import Component from '@ember/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class Badge extends Component {
+  tagName = ''
+
   @tracked
   text = null;
 }
